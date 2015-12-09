f327e00d48ece512e6a21100a4b48d7b22235769
Victor Queiroz <victorcqueirozg@gmail.com>
2015-12-09 01:24:27 -0300

    chore(src/Watcher.js): Watcher now only need three methods to get itself working, also the entirely design of the class is turned to dirty checking approach, so I tought that is a quite more performatic than Object.observe, which unfortunately (or not) has been withdrawn for the ES7 proposal

--- src/Watcher.js
+++ src/Watcher.js
@@ -1,6 +1,42 @@
 function Watcher () {
 	EventEmitter.call(this);
+
+	this.observer = new Observer(this);
 }
 inherits(Watcher, EventEmitter, {
-	
+	deliverChangeRecords: function() {
+		this.observer.deliverChangeRecords();
+	},
+
+	watch: function(exp, listener) {
+		var objectPath = exp.split('.');
+
+		objectPath = objectPath.slice(0, -1);
+
+		if(objectPath.length && (objectPath = objectPath.join('.'))) {
+			if(!has(this, objectPath)) {
+				set(this, objectPath, {});
+			}
+		}
+
+		var firstListener = false;
+
+		if(!this.observer.watchers.hasOwnProperty(exp)) {
+			firstListener = true;
+		}
+
+		this.observer.watch(exp, listener);
+
+		if(firstListener) {
+			this.observer.fire(exp);
+		}
+	},
+
+	watchGroup: function(exps, listener) {
+		var observer = this.observer;
+
+		forEach(exps, function(exp) {
+			observer.watch(exp, listener);
+		});
+	}
 });
--- test/Watcher.js
+++ test/Watcher.js
@@ -9,63 +9,22 @@ describe('Watcher', function() {
 		it('should watch a property', function() {
 			var listenerSpy = jasmine.createSpy();
 
-			watcher.$watch('somePropertyHere', listenerSpy);
+			watcher.watch('somePropertyHere', listenerSpy);
 			watcher.deliverChangeRecords();
 
-			expect(listenerSpy).toHaveBeenCalledWith(undefined);
+			expect(listenerSpy).toHaveBeenCalledWith(undefined, undefined);
 
 			watcher.somePropertyHere = [];
 			watcher.deliverChangeRecords();
-			expect(listenerSpy).toHaveBeenCalledWith([]);
+			expect(listenerSpy).toHaveBeenCalledWith([], undefined);
 
 			watcher.somePropertyHere = null;
 			watcher.deliverChangeRecords();
-			expect(listenerSpy).toHaveBeenCalledWith(null);
+			expect(listenerSpy).toHaveBeenCalledWith(null, []);
 
 			watcher.somePropertyHere = [];
 			watcher.deliverChangeRecords();
-			expect(listenerSpy).toHaveBeenCalledWith([]);
-		});
-	});
-
-	describe('$watchCollection()', function() {
-		it('should watch an object', function() {
-			var listenerSpy = jasmine.createSpy();
-
-			watcher.someDeep = {
-				objectRightHere: null
-			};
-			watcher.deliverChangeRecords();
-
-			watcher.$watchCollection('someDeep.objectRightHere', listenerSpy);
-			watcher.deliverChangeRecords();
-
-			watcher.someDeep.objectRightHere = {};
-			watcher.deliverChangeRecords();
-
-			watcher.someDeep.objectRightHere = {
-				someChangeHere: {}
-			};
-			watcher.deliverChangeRecords();
-
-			expect(listenerSpy).toHaveBeenCalledWith({});
-			expect(listenerSpy).toHaveBeenCalledWith({
-				someChangeHere: {}
-			});
-
-			watcher.someDeep = {
-				objectRightHere: null
-			};
-			watcher.deliverChangeRecords();
-
-			expect(listenerSpy).toHaveBeenCalledWith(null);
-
-			watcher.someDeep.objectRightHere = {somethingChanged: 1};
-			watcher.deliverChangeRecords();
-
-			expect(listenerSpy).toHaveBeenCalledWith({
-				somethingChanged: 1
-			});
+			expect(listenerSpy).toHaveBeenCalledWith([], null);
 		});
 	});
 });
