3ebdc75bee2cc8a410a9ec4cc882286e3a7b0a02
Victor Queiroz <victorcqueirozg@gmail.com>
2015-12-09 01:22:29 -0300

    chore(src/Observer.js): Observer service rewritten for a dirty checking design on Scope and Watcher

--- src/Observer.js
+++ src/Observer.js
@@ -1,195 +1,61 @@
-extend(Observer, {
-	UPDATE: 'update',
-	DELETE: 'delete',
-	ADD: 'add',
-
-	DELIVERING: 1,
-	IDLE: 2
-});
-
 function Observer(object) {
-	EventEmitter.call(this);
-
-	if(!isObject(object)) {
-		this.throwError('The first parameter must be an object');
-	}
-
-	this.state = Observer.IDLE;
 	this.object = object;
-	this.listeners = {};
-
-	var self = this,
-			listeners = this.listeners;
-
-	this.onChangeListener_ = function(changes) {
-		var i,
-				j,
-				prop,
-				change,
-				listener,
-				newValue,
-				oldValue;
-
-		for(i = 0; i < changes.length; i++) {
-			change = changes[i];
-			prop = change.name;
-			newValue = change.object[prop];
-
-			if(change.type == Observer.UPDATE && change.hasOwnProperty('oldValue')) {
-				oldValue = change.oldValue;
-			} else if(oldValue) {
-				oldValue = undefined;
-			}
-
-			if(listeners.hasOwnProperty(prop) && listeners[prop].length) {
-				for(j = 0; j < listeners[prop].length; j++) {
-					listener = listeners[prop][j];
-
-					listener(newValue, oldValue);
-				}
-			}
-		}
-
-		self.emit('update', changes);
-	};
-
-	this.observerListener_ = observe(object, this.onChangeListener_);
+	this.watchers = {};
 }
 
-inherits(Observer, EventEmitter, {
-	throwError: function(msg) {
-		throw new Error(msg);
-	},
-
-	watch: function(property, listener) {
-		if(!this.listeners.hasOwnProperty(property)) {
-			this.listeners[property] = [];
-		}
-
-		this.listeners[property].push(listener);
-		this.deliverChangeRecords();
-	},
-
+Observer.prototype = {
 	deliverChangeRecords: function() {
-		Object.deliverChangeRecords(this.observerListener_);
-	}
-});
-
-function observe(object, listener) {
-	Object.observe(object, listener);
-
-	return listener;
-}
-
-function unobserve(object, listener) {
-	Object.unobserve(object, listener);
-}
-
-function DeepObserver(object, parentObserver, parentProperty) {
-	Observer.call(this, object);
-
-	this.childObservers = {};
-
-	if(parentObserver instanceof DeepObserver === true) {
-		this.parentObserver = parentObserver;
-	}
-
-	if(isString(parentProperty)) {
-		this.parentProperty = parentProperty;
-	}
-
-	if(this.parentObserver && !this.parentProperty) {
-		this.throwError('A child observer must have a parent property');
-	}
-
-	this.on('update', function(changes) {
-		var i = 0,
-				ii = changes.length,
-				prop,
+		var i,
+				ii,
+				path,
+				keys = Object.keys(this.watchers),
 				value,
-				change;
+				watcher;
 
-		for(; i < ii; i++) {
-			change = changes[i];
-			prop = change.name;
-			value = change.object[prop];
+		for(i = 0, ii = keys.length; i < ii; i++) {
+			path = keys[i];
+			value = get(this.object, path);
+			watcher = this.watchers[path]
+			oldValue = watcher.oldValue;
 
-			if(change.type == Observer.DELETE || change.type == Observer.UPDATE) {
-				if(isObject(change.oldValue) || isArray(value)) {
-					if(this.childObservers.hasOwnProperty(prop)) {
-						this.childObservers[prop].destroy();
-					}
-				}
+			if(isObject(value) && !isEqual(value, oldValue)) {
+				this.fire(path);
+			} else if (value !== oldValue) {
+				this.fire(path);
 			}
 
-			if(change.type == Observer.UPDATE || change.type == Observer.ADD) {
-				if(isObject(value) || isArray(value)) {
-					this.createChildObserver(prop, value);
-				}
-			}
+			watcher.oldValue = clone(value);
 		}
-	})
-	.on('pathChanged', function(path, value, oldValue) {
-		if(this.parentObserver) {
-			path = path.split('.');
-			path.unshift(this.parentProperty);
-
-			this.parentObserver.emit('pathChanged', path.join('.'), value, oldValue);
-		}
-	});
-}
-
-inherits(DeepObserver, Observer, {
-	createChildObserver: function (property, object) {
-		if(this.childObservers.hasOwnProperty(property)) {
-			this.childObservers[property].destroy();
-		}
-
-		var parentObserver = this;
-		var childObserver = new DeepObserver(object, parentObserver, property);
-
-		childObserver.on('update', function(changes) {
-			var i = 0,
-					ii = changes.length,
-					path,
-					value,
-					change;
-
-			for(; i < ii; i++) {
-				path = [];
-				change = changes[i];
-
-				path.push(property, change.name);
-				value = change.object[change.name];
-
-				parentObserver.emit('pathChanged', path.join('.'), value, change.oldValue);
-			}
-		})
-		.on('destroy', function() {
-			delete parentObserver.childObservers[property];
-		});
-
-		this.childObservers[property] = childObserver;
 	},
 
-	watch: function (path, listener) {
-		//
-	},
+	watch: function(path, listener) {
+		var watcher,
+				listeners;
 
-	destroy: function() {
-		unobserve(this.object, this.onChangeListener_);
-		this.emit('destroy');
-		this.removeAllListeners();
-	},
-
-	deliverChangeRecords: function(ignoreChilds) {
-		var i = 0,
-				observers = Object.keys(this.childObservers);
-
-		for(; i < observers.length; i++) {
-			this.childObservers[observers[i]].deliverChangeRecords();
+		if(!this.watchers.hasOwnProperty(path)) {
+			this.watchers[path] = watcher = {
+				path: path,
+				oldValue: undefined,
+				listeners: []
+			};
+		} else {
+			watcher = this.watchers[path];
 		}
 
-		Observer.prototype.deliverChangeRecords.call(this, arguments);
+		listeners = watcher.listeners;
+		listeners.push(listener);
+	},
+
+	fire: function(path) {
+		var i,
+				ii,
+				watcher = this.watchers[path],
+				listeners = watcher.listeners;
+
+		for(i = 0, ii = listeners.length; i < ii; i++) {
+			listeners[i](get(this.object, watcher.path), watcher.oldValue);
+		}
+
+		return this;
 	}
-});
+};
