eb1f539d8d6873a96d299baf18487f764a089f22
Victor Queiroz <victorcqueirozg@gmail.com>
2015-12-09 01:27:06 -0300

    chore(test/Scope.js): Using the new scope `watch()` method and tests passing with the new and correct watcher spy call testing

--- test/Scope.js
+++ test/Scope.js
@@ -4,34 +4,35 @@ describe('Scope', function() {
 	beforeEach(function() {
 		scope = new Scope();
 	});
+	
   describe('watch()', function() {
     it('should watch a property', function() {
       var listenerSpy = jasmine.createSpy();
-      scope.$watch('someProperty', listenerSpy);
+      scope.watch('someProperty', listenerSpy);
 
       scope.someProperty = 'someValueHere';
       scope.deliverChangeRecords();
 
-      expect(listenerSpy).toHaveBeenCalledWith('someValueHere');
+      expect(listenerSpy).toHaveBeenCalledWith('someValueHere', undefined);
     });
 
     it('should deep properties', function() {
       var watcherSpy = jasmine.createSpy();
 
-      scope.$watch('another.deep.property.here', watcherSpy);
+      scope.watch('another.deep.property.here', watcherSpy);
       scope.deliverChangeRecords();
 
-      expect(watcherSpy).toHaveBeenCalledWith(undefined);
+      expect(watcherSpy).toHaveBeenCalledWith(undefined, undefined);
 
       scope.another.deep.property.here = 0;
       scope.deliverChangeRecords();
 
-      expect(watcherSpy).toHaveBeenCalledWith(0);
+      expect(watcherSpy).toHaveBeenCalledWith(0, undefined);
 
       scope.another.deep.property.here += 1;
       scope.deliverChangeRecords();
 
-      expect(watcherSpy).toHaveBeenCalledWith(1);
+      expect(watcherSpy).toHaveBeenCalledWith(1, 0);
     });
   });
 });
