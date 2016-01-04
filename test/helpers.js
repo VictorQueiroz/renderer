fdaad073910144f937ae0c1b136a92da1bdbb24e
Victor Queiroz <victorcqueirozg@gmail.com>
2016-01-04 05:15:04 -0300

    chore(test/helpers.js): More reliable tests to guarantee that `clone()` function is working as it should

--- test/helpers.js
+++ test/helpers.js
@@ -16,6 +16,7 @@ describe('Helpers', function() {
 
 		expect(isEqual({ a: 1 }, { a: 1	})).toBeTruthy();
 		expect(isEqual({ a: 1 }, { b: 1	})).not.toBeTruthy();
+    expect(isEqual({},{array:[],object:{}})).not.toBeTruthy();
 
 		expect(isEqual({
 			someDeepObject: {
@@ -39,15 +40,24 @@ describe('Helpers', function() {
 			}
 		})).toBeTruthy();
 		expect(isEqual({ a: 1 }, { b: 1	})).not.toBeTruthy();
+    expect(isEqual(true, true)).toBeTruthy();
+    expect(isEqual(true, false)).toBeFalsy();
 	});
 
   it('should clone a deep array', function() {
     expect(clone([[[[]]]])).toEqual([[[[]]]]);
+    expect(clone([[[[]]],[[[],[]]]])).toEqual([[[[]]],[[[],[]]]]);
   });
 
   it('should clone an object', function() {
     var object = {
       huge: {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+
         object: {
           lots: {
             of: {
@@ -57,12 +67,32 @@ describe('Helpers', function() {
             }
           }
         }
-      }
+      },
+      empty: {},
+      array1: [],
+      array2: [],
+      array3: [],
+      array4: []
     };
 
     expect(object).toBe(object);
     expect(clone(object)).toEqual(object);
+    expect(clone(object)).toEqual(clone(object));
     expect(clone(object)).not.toBe(object);
+
+    object = {array:[]};
+    var clonedObject = clone(object);
+
+    expect(clonedObject).not.toBe(object);
+
+    object.array.push(0);
+    expect(clonedObject).toEqual({array:[]});
+  });
+
+  it('should clone numbers', function() {
+    for(var i = 0; i < 20; i++) {
+      expect(clone(i)).toBe(i);
+    }
   });
 
   it('should create custom error message', function() {
