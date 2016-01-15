5e85990beb1284d3ebf11690c9017a16557ffab5
Victor Queiroz <victorcqueirozg@gmail.com>
2016-01-15 13:56:14 -0300

    chore(*): WIP - compile function rewritted to a more performatic approach, the idea is get other parts of the compile bit by bit to give us time to rethink about each feature and approach

---
+++ src/compile.js
@@ -0,0 +1,201 @@
+var registry = directiveRegistry;
+
+function compile(node) {
+  var nodes;
+
+  if(isArray(node) || (node instanceof NodeList === true)) {
+    nodes = node;
+    node = null;
+  } else {
+    nodes = [];
+    nodes.push(node);
+  }
+
+  var compositeLinkFn = compileNodes(nodes);
+
+  return function(scope) {
+    if(compositeLinkFn) {
+      compositeLinkFn(scope);
+    }
+  };
+}
+
+/**
+ * Apply a set of directives to a node element:
+ *  - Execute the compile function of all the directives instances
+ *  - Store the retrieved linking functions from the execution of the compile function
+ *
+ * Returns the node link function
+ */
+function apply(directives, node, attributes) {
+  var i,
+      ii,
+      linkFn,
+      directive;
+
+  // pre/post links
+  var preLinkFns = [],
+      postLinkFns = [];
+
+  for(i = 0, ii = directives.length; i < ii; i++) {
+    directive = directives[i];
+
+    linkFn = directive.compile(node, attributes);
+
+    if(isFunction(linkFn)) {
+      addLinkFn(0, linkFn);
+    } else if(isObject(linkFn)) {
+      addLinkFn(linkFn.pre, linkFn.post);
+    }
+  }
+
+  function addLinkFn (pre, post, directive) {
+    if(pre) {
+      preLinkFns.push(pre);
+    }
+
+    if(post) {
+      postLinkFns.push(post);
+    }
+  }
+
+  /**
+   *  - Executes the pre and post linking functions
+   */
+  return function nodeLinkFn (scope, childLinkFn) {
+    var i;
+
+    for(i = 0; i < preLinkFns.length; i++) {
+      invokeLinkFn(preLinkFns[i],
+        scope,
+        node,
+        attributes
+      );
+    }
+
+    if(childLinkFn) {
+      childLinkFn(scope, node.childNodes);
+    }
+
+    for(i = postLinkFns.length - 1; i >= 0; i--) {
+      invokeLinkFn(postLinkFns[i],
+        scope,
+        node,
+        attributes
+      );
+    }
+  };
+}
+
+function invokeLinkFn(linkFn, scope, node, attributes) {
+  linkFn(scope, node, attributes);
+}
+
+function addDirective(name, directives) {
+  var i,
+      ii,
+      instances;
+
+  if((instances = registry.$$get(name))) {
+    for(i = 0, ii = instances.length; i < ii; i++) {
+      directives.push(instances[i]);
+    }
+  }
+}
+
+function scan(node, directives, attributes) {
+  var i,
+      ii,
+      name = camelCase(node.tagName),
+      attr,
+      attrs = node.attributes;
+
+  addDirective(name, directives);
+
+  for(i = 0, ii = attrs.length; i < ii; i++) {
+    attr = attrs[i];
+    name = camelCase(attr.name);
+
+    attributes[name] = attr.value;
+  }
+}
+
+function compileNodes(nodeList) {
+  var i,
+      linkFns = [],
+      childLinkFn,
+      linkFnFound,
+      nodeLinkFnFound;
+
+  // node link function variables
+  var directives,
+      nodeLinkFn,
+      attributes;
+
+  for(i = 0; i < nodeList.length; i++) {
+    directives = [],
+    attributes = new Attributes();
+
+    scan(nodeList[i], directives, attributes);
+
+    nodeLinkFn = directives.length ? apply(directives, nodeList[i], attributes) : null;
+
+    // Check if the node link is terminal or have child nodes to be compiled.
+    // If it's a terminal node link function it means that a set of directives
+    // must be compiled before the rest of them, according to the priority level
+    // The highest priority level directive will be compiled first (as it should)
+    // until it reach the terminal directive instance which generates a transclude
+    // function to be executed later
+    childLinkFn = !(nodeLinkFn && nodeLinkFn.terminal) &&
+                  nodeList[i] &&
+                  nodeList[i].childNodes &&
+                  nodeList[i].childNodes.length;
+
+    childLinkFn = childLinkFn ? compileNodes(nodeList[i].childNodes) : null;
+
+    if(childLinkFn || nodeLinkFn) {
+      linkFnFound = true;
+      nodeLinkFnFound = nodeLinkFnFound || nodeLinkFn;
+
+      linkFns.push(i, nodeLinkFn, childLinkFn);
+    }
+  }
+
+  return (linkFnFound ? compositeLinkFn : null);
+
+  function compositeLinkFn (scope, nodeList) {
+    var i,
+        node,
+        nodeLinkFn,
+        childLinkFn,
+        stableNodeList;
+
+    // copy node list so if the node link adds or remove a dom element our
+    // offsets don't get screw up
+    if(nodeLinkFnFound) {
+      var idx,
+          nodeListLength = nodeList.length;
+
+      stableNodeList = new Array(nodeListLength);
+
+      for(i = 0; i < linkFns.length; i += 3) {
+        idx = linkFns[i];
+        stableNodeList[idx] = nodeList[idx];
+      }
+    } else {
+      stableNodeList = nodeList;
+    }
+
+    for(i = 0; i < linkFns.length;) {
+      node = stableNodeList[linkFns[i++]];
+      nodeLinkFn = linkFns[i++];
+      childLinkFn = linkFns[i++];
+
+      if(nodeLinkFn) {
+        nodeLinkFn(scope, childLinkFn);
+      } else if (childLinkFn) {
+        childLinkFn(scope, node.childNodes);
+      }
+    }
+  }
+}
---
+++ test/compile.js
@@ -0,0 +1,218 @@
+var createNode = function(html) {
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  return div;
+};
+
+describe('compile()', function() {
+  var node,
+      clear,
+      register,
+      noop_directive;
+
+  beforeEach(function() {
+    noop_directive = function(i) {
+      return function() {
+        var name = 'noop_directive_' + i;
+
+        return {name: name, compile: noop};
+      };
+    };
+
+    register = function() {
+      return renderer.register.apply(renderer, arguments);
+    };
+  });
+
+  afterEach(function() {
+    for(var name in registry) {
+      if(name == '$$get' || typeof registry[name] == 'function') {
+        continue;
+      }
+
+      delete registry[name];
+    }
+  });
+
+  describe('scan()', function() {
+    var directives,
+        attributes;
+
+    beforeEach(function() {
+      directives = [],
+      attributes = new Attributes();
+    });
+
+    it('should match directives by the tag name', function() {
+      node = document.createElement('my-component');
+      for(var i = 0; i < 10; i++) {
+        register('myComponent', noop_directive(i));
+      }
+
+      scan(node, directives, attributes);
+
+      for(var i = 0; i < 10; i++) {
+        expect(directives[i].name).toEqual('noop_directive_' + i);
+        expect(directives[i].compile).toEqual(noop);
+      }
+    });
+
+    it('should store the node attributes in the "attributes" argument', function() {
+      node = document.createElement('my-component');
+      node.setAttribute('component-data', '{ users: appCtrl.users }');
+
+      scan(node, directives, attributes);
+
+      expect(attributes.componentData).toBe('{ users: appCtrl.users }');
+    });
+  });
+
+  describe('compileNodes()' ,function() {
+    var node,
+        scope;
+
+    beforeEach(function() {
+      node = createNode(''),
+      scope = new Scope();
+
+      for(var i = 0; i < 20; i++) {
+        node.appendChild(document.createElement('my-single-component'));
+      }
+    });
+
+    it('should return "null" if no directives is found', function() {
+      expect(compileNodes(node.childNodes)).toBeNull();
+
+      register('mySingleComponent', noop_directive);
+      expect(compileNodes(node.childNodes).name).toBe('compositeLinkFn');
+    });
+
+    it('should execute all the node list node linking functions', function() {
+      var postLinkSpy = jasmine.createSpy(),
+          preLinkSpy = jasmine.createSpy();
+
+      register('mySingleComponent', function() {
+        return {
+          compile: function(element) {
+            element.setAttribute('compiled', 'true');
+
+            return {pre: preLinkSpy, post: postLinkSpy};
+          }
+        }
+      });
+
+      compileNodes(node.childNodes)(scope, node.childNodes);
+
+      var nodes = node.querySelectorAll('my-single-component');
+
+      for(var i = 0; i < nodes.length; i++) {
+        expect(nodes[i].getAttribute('compiled')).toEqual('true');
+      }
+
+      expect(preLinkSpy).toHaveBeenCalled();
+      expect(postLinkSpy).toHaveBeenCalled();
+    });
+
+    it('should compile elements added at "compile" time normally', function() {
+      var postLinkSpy = jasmine.createSpy();
+
+      register('mySingleComponent', function() {
+        return {
+          compile: function(element) {
+            element.parentNode.insertBefore(document.createElement('div'), element.nextSibling);
+
+            return postLinkSpy;
+          }
+        }
+      });
+
+      register('div', function() {
+        return {
+          link: function(scope, element) {
+            element.setAttribute('div-compiled', 'true');
+          }
+        };
+      });
+
+      compileNodes(node.childNodes)(scope, node.childNodes);
+
+      var nodes = node.querySelectorAll('my-single-component');
+
+      for(var i = 0; i < nodes.length; i++) {
+        expect(nodes[i].nextSibling.getAttribute('div-compiled')).toEqual('true');
+      }
+
+      expect(postLinkSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('apply()', function() {
+    var node,
+        directives,
+        attributes;
+
+    beforeEach(function() {
+      directives = [],
+      attributes = new Attributes();
+    });
+
+    it('should execute the compile function', function() {
+      var compileSpy = jasmine.createSpy();
+      register('div', function() {
+        return {compile: compileSpy};
+      });
+
+      node = createNode();
+      scan(node, directives);
+      apply(directives, node, attributes);
+
+      expect(compileSpy).toHaveBeenCalledWith(node, attributes);
+    });
+
+    describe('nodeLinkFn()', function() {
+      var node,
+          scope,
+          preLinkSpy,
+          nodeLinkFn,
+          postLinkSpy,
+          childLinkFn;
+
+      beforeEach(function() {
+        node = createNode('<div></div>'),
+        scope = new Scope(),
+        directives = [],
+        preLinkSpy = jasmine.createSpy(),
+        postLinkSpy = jasmine.createSpy();
+
+        register('div', function() {
+          return {compile: function() {
+            return {
+              post: postLinkSpy,
+              pre: preLinkSpy
+            }
+          }};
+        });
+
+        childLinkFn = jasmine.createSpy();
+      });
+
+      it('should execute all the post/pre linking functions', function() {
+        scan(node, directives);
+
+        nodeLinkFn = apply(directives, node, attributes);
+        nodeLinkFn(scope);
+
+        expect(postLinkSpy).toHaveBeenCalledWith(scope, node, attributes);
+      });
+
+      it('should execute a child link function if provide one', function() {
+        scan(node, directives);
+
+        nodeLinkFn = apply(directives, node, attributes);
+        nodeLinkFn(scope, childLinkFn);
+
+        expect(childLinkFn).toHaveBeenCalled();
+      });
+    });
+  });
+});
