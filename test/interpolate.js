5eb4742f56ed4af403f6a1b70a241aecf725d9c4
Victor Queiroz <victorcqueirozg@gmail.com>
2016-01-18 10:38:39 -0300

    chore(src/interpolate.js): interpolate function functionality implemented

--- test/interpolate.js
+++ test/interpolate.js
@@ -7,4 +7,29 @@ describe('interpolate()', function() {
     expect(interpolateFn.exp).toBe('some text');
     expect(interpolateFn.expressions).toEqual([]);
   });
+
+  it('should match expressions between {{ and }}', function() {
+    interpolateFn = interpolate('some text and {{ exp }}');
+
+    expect(interpolateFn.expressions).toEqual(['exp']);
+  });
+
+  it('should compile expressions', function() {
+    var locals = {expHere: fn};
+
+    function fn () {
+      return 'cool value';
+    }
+
+    interpolateFn = interpolate('some text and some {{ expHere() }}');
+
+    expect(interpolateFn(locals)).toEqual('some text and some cool value');
+  });
+
+  it('should accept another interpolation symbols', function() {
+    var interpolateFn = interpolate('interpolate this -> [[ value ]]', {startSymbol: '[[', endSymbol: ']]'}),
+        value = '"value here"';
+
+    expect(interpolateFn({value: value})).toEqual('interpolate this -> "value here"');
+  });
 });
